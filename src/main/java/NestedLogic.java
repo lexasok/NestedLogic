e5f15bfcbae6f92d9a00d0619471c51d23019c61
lexa <sokolov@ozero.net>
2018-05-04 21:01:26 +0300

    New class added

--- src/main/java/NestedLogic.java
+++ src/main/java/NestedLogic.java
@@ -2,7 +2,20 @@ import java.util.Scanner;
 
 public class NestedLogic {
 
-
     public static void main(String[] args) {
+
     }
 }
+
+class BookReturnDate {
+    int day;
+    int month;
+    int year;
+
+    public BookReturnDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}
+
