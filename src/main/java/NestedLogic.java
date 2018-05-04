44354e284b0d7fbaf66dcba74c596edf860e4d7d
lexa <sokolov@ozero.net>
2018-05-04 21:09:46 +0300

    Mistake corrected

--- src/main/java/NestedLogic.java
+++ src/main/java/NestedLogic.java
@@ -5,16 +5,16 @@ public class NestedLogic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BookReturnDate actual = new BookReturnDate(sc.nextInt(), sc.nextInt(), sc.nextInt());
-        BookReturnDate expexted = new BookReturnDate(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        BookReturnDate expected = new BookReturnDate(sc.nextInt(), sc.nextInt(), sc.nextInt());
 
         int fine = 0;
-        if (actual.year == expexted.year) {
-            if (actual.month > expexted.month) {
-                fine = (actual.month - expexted.month) * 500;
-            } else if (actual.month == expexted.month && actual.day > expexted.day) {
-                fine = (actual.day - expexted.day) * 15;
+        if (actual.year == expected.year) {
+            if (actual.month > expected.month) {
+                fine = (actual.month - expected.month) * 500;
+            } else if (actual.month == expected.month && actual.day > expected.day) {
+                fine = (actual.day - expected.day) * 15;
             }
-        } else if (expexted.year < actual.year) {
+        } else if (expected.year < actual.year) {
             fine = 10000;
         }
         System.out.println(fine);
