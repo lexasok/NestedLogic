a4448e9129163d6e1450867693c4d933dec14ac7
lexa <sokolov@ozero.net>
2018-05-04 20:22:00 +0300

    Data input implemented

--- src/main/java/NestedLogic.java
+++ src/main/java/NestedLogic.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class NestedLogic {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Scanner sc = new Scanner(System.in);
+        int dayReturned, monthReturned, yearReturned;
+        int dayExpected, monthExpected, yearExpected;
+
+        //Returned
+        dayReturned = sc.nextInt();
+        monthReturned = sc.nextInt();
+        yearReturned = sc.nextInt();
+
+        //Expected
+        dayExpected = sc.nextInt();
+        monthExpected = sc.nextInt();
+        yearExpected = sc.nextInt();
+
+        sc.close();
     }
 }
