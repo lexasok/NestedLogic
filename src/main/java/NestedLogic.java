d583855a090c15dfc7f72f0fdbf899f269ad2e04
lexa <sokolov@ozero.net>
2018-05-04 21:08:19 +0300

    Solution done

--- src/main/java/NestedLogic.java
+++ src/main/java/NestedLogic.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 public class NestedLogic {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BookReturnDate actual = new BookReturnDate(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        BookReturnDate expexted = new BookReturnDate(sc.nextInt(), sc.nextInt(), sc.nextInt());
 
+        int fine = 0;
+        if (actual.year == expexted.year) {
+            if (actual.month > expexted.month) {
+                fine = (actual.month - expexted.month) * 500;
+            } else if (actual.month == expexted.month && actual.day > expexted.day) {
+                fine = (actual.day - expexted.day) * 15;
+            }
+        } else if (expexted.year < actual.year) {
+            fine = 10000;
+        }
+        System.out.println(fine);
     }
 }
 
